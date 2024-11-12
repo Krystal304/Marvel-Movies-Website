d265e2828b36843488e88cc31f6e4ddbe503739e
Krystal304 <krystalwilliams864@gmail.com>
2024-11-11 21:24:03 -0500

    search movie component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { useState } from 'react'
 import './App.css'
 import SearchMovies from './component/SearchMovies'
 import FavoritesList from './component/FavoritesList'
-import axios from 'axios'
+
 function App() {
   const [favorites, setFavorites] = useState([])
+
+  const apiKey = 'cabd0d98a3cb74aab336410d9d94ded0';
 
   const addFavoriteMovie = (movie) => {
     setFavorites([...favorites, movie]);
--- src/component/SearchMovies.jsx
+++ src/component/SearchMovies.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
-function SearchMovies() {
-  return (
-    <div>SearchMovies</div>
-  )
+import { useState } from 'react'
+import axios from 'axios'
+
+
+const SearchMovies = ({addFavoriteMovie}) => {
+    const apiKey = 'cabd0d98a3cb74aab336410d9d94ded0';
+    const [searchMovie, setSearchMovie] = useState('')
+    const [movies, setMovies] = useState([]);
+
+const getMovies = async () => {
+    try {
+        const response = await fetch(`developer.marvel.com/?apikey=${apiKey}&s=${searchMovie}&type=movie`)
+        const data = await response.json();
+        setMovies(data.Search || []);
+    } catch (err) {
+        console.error("Error Fetching Movie",err);
+        
+    }
 }
 
+return (
+    <div>
+        <h2>Search Movies</h2>
+        <input type="text" value={searchMovie} onChange={(e) => setSearchMovie(e.target.value)}/>
+        <button onClick={getMovies}>Search</button>
+        <ul>
+            {movies.map((movie) => (
+                <li key={movie.imdbID}>
+                    {movie.Title} ({movie.Year})
+                    <button onClick={() => addFavoriteMovie(movie)}>Add to Favorites</button>
+                </li>
+            ))}
+        </ul>
+    </div>
+)
+
+}
 export default SearchMovies
